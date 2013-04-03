0bc48c6cc7780ea7c712ea39766be1d5a1dab98d
Sean Moubry <sean@moubry.com>
2013-04-02 20:06:38 -0500

    Implementation first pass

---
+++ 8.rb
@@ -0,0 +1,33 @@
+# a word is at least two letters
+
+class ConcatWordFinder
+
+  def self.find(words)
+    words.each do |word|
+      if is_word?(word)
+        leads = find_words_this_begins_with(word, words)
+        puts 'the leads for ' + word + ': ' + leads.to_s
+      end
+    end
+  end
+
+  private
+
+    def self.find_words_this_begins_with(word, words)
+      words.select { |w| w.start_with?(word) }
+    end
+
+    def self.is_word?(word)
+      word.length >= 2
+    end
+
+end
+
+dictionary = [
+  "a",
+  "al",
+  "bums",
+  "albums"
+]
+
+ConcatWordFinder.find(dictionary)
