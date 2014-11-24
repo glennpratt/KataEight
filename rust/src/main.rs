58fd386e57a0370623950df2116279884b09ac05
Glenn Pratt <glenn.pratt@acquia.com>
2014-11-24 12:26:06 -0600

    Make the Rust code OO style and add timings.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,47 +1,78 @@
-extern crate test;
+extern crate time;
 
 use std::collections::HashSet;
 use std::io::BufferedReader;
 use std::io::File;
-use test::Bencher;
+use time::precise_time_ns;
 
-
-fn main() {
-    let path = Path::new("assets/wordsEn.txt");
-    let dict = load_dictionary(path);
-    let matches = find(dict);
-    println!("{}", matches);
+pub struct Dictionary {
+    dict: HashSet<String>,
 }
 
-fn load_dictionary(path: Path) -> HashSet<String> {
-    let mut dict = HashSet::new();
-    let mut file = BufferedReader::new(File::open(&path));
-    for line in file.lines() {
-        dict.insert(line.unwrap().trim().to_string());
-    }
-    dict
-}
-
-fn find(dict: HashSet<String>) -> Vec<String> {
-    let mut matches: Vec<String> = Vec::new();
-    for word in dict.iter() {
-        if (word.len() != 6) { continue }
-        for i in range(2u, 5) {
-          if dict.contains(word.slice_to(i)) && dict.contains(word.slice_from(i)) {
-            matches.push(word.clone());
-            break
-          }
+impl Dictionary {
+    pub fn load_from_file(path: &Path) -> Dictionary {
+        let mut dict = HashSet::new();
+        let mut file = BufferedReader::new(File::open(path));
+        for line in file.lines() {
+            dict.insert(line.unwrap().trim().to_string());
         }
+        Dictionary {dict: dict}
     }
-    matches
+
+    pub fn words_of_two(&self) -> Vec<&String> {
+        let mut matches: Vec<&String> = Vec::new();
+        for word in self.dict.iter() {
+            if word.len() != 6 { continue  }
+            for i in range(2u, 5) {
+                if self.dict.contains(word.slice_to(i)) &&
+                       self.dict.contains(word.slice_from(i)) {
+                    matches.push(word);
+                    break
+                }
+            }
+        }
+        matches
+    }
 }
 
-fn foo(bar: int) -> int {
-  bar + 1
+pub fn main() {
+    println!("Started...");
+    let start = precise_time_ns();
+    let path = Path::new("assets/wordsEn.txt");
+    let dict = Dictionary::load_from_file(&path);
+    let loaded = precise_time_ns();
+    let words = dict.words_of_two();
+    let search = precise_time_ns();
+    println!("Found:  {} words" , words.len());
+    println!("Load:   {} ns" , loaded - start);
+    println!("Search: {} ns" , search - loaded);
+    println!("Total:  {} ns" , search - start);
 }
 
-#[bench]
-fn bench_find(b: &mut Bencher) {
-    let bar = 1i;
-    b.iter(|| foo(bar));
+#[cfg(test)]
+mod test_bench {
+    extern crate test;
+
+    use Dictionary;
+    use self::test::Bencher;
+
+    #[test]
+    fn should_find_n_words_of_two() {
+        let path = Path::new("assets/wordsEn.txt");
+        let dict = Dictionary::load_from_file(&path);
+        assert_eq!(3715u , dict.words_of_two().len());
+    }
+
+    #[bench]
+    fn bench_words_of_two(b: &mut Bencher) {
+        let path = Path::new("assets/wordsEn.txt");
+        let dict = Dictionary::load_from_file(&path);
+        b.iter(|| dict.words_of_two());
+    }
+
+    #[bench]
+    fn bench_load_dictionary(b: &mut Bencher) {
+        let path = Path::new("assets/wordsEn.txt");
+        b.iter(|| Dictionary::load_from_file(&path));
+    }
 }
